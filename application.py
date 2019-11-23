58be74800cd3bfac1e4e796e5cd5639fd589dadf
Cristopher Dávila <cristopher.d.96@gmail.com>
2019-11-22 21:32:25 -0500

    requirements

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
 import os
-
+from os.path import join, dirname
 from flask import Flask
 from flask_socketio import SocketIO, emit
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')  # Path to .env file
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
